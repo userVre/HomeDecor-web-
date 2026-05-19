bb6b10f556d0179e1043a8a2566f0adf81d55b63
userVre <123872204+userVre@users.noreply.github.com>
2026-05-19 19:34:46 +0100

    Use static XML sitemap

--- app/sitemap.ts
+++
@@ -1,6 +0,0 @@
-import type { MetadataRoute } from "next";
-import { getSitemapEntries } from "@/lib/sitemap";
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return getSitemapEntries();
-}
